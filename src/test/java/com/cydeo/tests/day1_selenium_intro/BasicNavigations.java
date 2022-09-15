ce79294cbdb2fa71f764d74d7efd03717fd45a80
furkangergin <hunter_1325@hotmail.com>
2022-09-15 19:55:18 +0300

    committed until day 9-10 configuration

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day1_selenium_intro;
 
+import com.cydeo.tests.utilities.Driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,8 +18,12 @@ public class BasicNavigations {
         //This line will maximize the browser size
         driver.manage().window().maximize();
 
+
+
         //3- Go to "https://www.tesla.com"
         driver.get("https://www.tesla.com");
+
+        //System.out.println("driver.getWindowHandle() = " + driver.getWindowHandle());
 
         //get the title of the page
         String currentTitle = driver.getTitle();
