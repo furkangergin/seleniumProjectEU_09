456eeafb543a45441edd44e3513f932aa0bc8b03
furkangergin <hunter_1325@hotmail.com>
2022-09-15 20:59:25 +0300

    committed TestBase2

--- src/test/java/com/cydeo/tests/base/TestBase.java
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -1,12 +1,10 @@
-package com.cydeo.base;
+package com.cydeo.tests.base;
 
-import com.cydeo.tests.utilities.BrowserUtils;
 import com.cydeo.tests.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import javax.sql.rowset.BaseRowSet;
 import java.util.concurrent.TimeUnit;
 
 public abstract class TestBase {
